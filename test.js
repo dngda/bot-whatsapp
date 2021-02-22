d3132f5a69a426c4ce24aeab3926c3e139e83c44
dngda <opowaekeno123@gmail.com>
2021-02-22 16:04:24 +0700

    add test

--- test.js
+++ test.js
@@ -1,16 +1,41 @@
-const scrapy = require('node-scrapy')
-const fetch = require('node-fetch')
+const wa = require('@open-wa/wa-automate')
+const options = require('./utils/options')
 
-const url = 'https://kbbi.web.id/'
-const kata = 'asdasd'
+wa.create(options(true, start))
+    .then(client => start(client))
+    .catch(err => new Error(err))
 
-const model = {
-  arti: 'div#d1 ($ | trim)'
+function start(client) {
+
+client.onMessage(async message => {
+	const { type, id, from, t, sender, author, isGroupMsg, chat, chatId, caption, isMedia, mimetype, quotedMsg, quotedMsgObj, mentionedJidList } = message
+	const stickerMetadata = { pack: 'Created with', author: 'SeroBot', keepScale: true }
+	const isQuotedImage = quotedMsg && quotedMsg.type === 'image'
+	body = (type === 'chat') ? body : ((type === 'image' && caption || type === 'video' && caption)) ? caption : ''
+
+    if (body === 'sticker' | body === 'stiker') {
+    	console.log('\nbody:' + body)
+    	await client.reply(from, `Copy that, processing...`, id)
+
+        const encryptMedia = isQuotedImage ? quotedMsg : message
+        console.log('\nencryptMedia:' + encryptMedia)
+
+        const _mimetype = isQuotedImage ? quotedMsg.mimetype : mimetype
+        console.log('\n_mimetype:' + _mimetype)
+
+        const mediaData = await wa.decryptMedia(encryptMedia)
+        console.log('\nmediaData:' + mediaData)
+
+        const imageBase64 = `data:${_mimetype};base64,${mediaData.toString('base64')}`
+        console.log('\nimageBase64:' + imageBase64)
+
+        client.sendImageAsSticker(from, imageBase64, stickerMetadata)
+            .then(() => {
+                client.sendText(from, 'Here\'s your sticker')
+            }).catch(err => {
+                console.log(err)
+                client.sendText(from, 'Maaf, ada yang error!')
+            })
+		}
+    })
 }
-
-fetch(url+kata)
-  .then((res) => res.text())
-  .then((body) => {
-    console.log(scrapy.extract(body, model))
-  })
-  .catch(console.error)
