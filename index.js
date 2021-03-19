c15f03d66507c305b3189518cce731c9ffd82739
dngda <opowaekeno123@gmail.com>
2021-03-19 20:34:56 +0700

    .

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ async function start(client = new Client()) {
         console.log(color('[~>>]', 'red'), `Someone is calling bot, lol~ id: ${call.peerJid}}`)
         // ketika seseorang menelpon nomor bot akan mengirim 
         if (!call.isGroup){
-            client.sendText(call.peerJid, 'Maaf tidak bisa menerima panggilan.\n\nIni robot, bukan manusia.\nAwas kena block!~')
+            client.sendText(call.peerJid, 'Maaf tidak bisa menerima panggilan.\n\nIni robot, bukan manusia.\nAwas kena block!~\nChat https://wa.me/6282310487958 for unblock request.')
             client.contactBlock(call.peerJid)
         }
     })
