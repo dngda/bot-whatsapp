080e6f8c07c8468e0ca8b3153eec3a514fd33fea
dngda <opowaekeno123@gmail.com>
2021-03-25 20:56:39 +0700

    check nobg api

---
+++ checkapinobg.js
@@ -0,0 +1,15 @@
+const request = require('request')
+const fs = require('fs')
+const { apiNoBg } = JSON.parse(fs.readFileSync('./settings/api.json'))
+
+apiNoBg.forEach(api => {
+	request.get({
+	  url: 'https://api.remove.bg/v1.0/account',
+	  headers: {
+	    'X-Api-Key': api
+	  },
+	  encoding: null
+	}, function(error, response, body) {
+	  console.log(api, JSON.stringify(JSON.parse(body).data.attributes.api))
+	})
+})
