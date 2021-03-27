d76ba638e27b06d353669d63c909d293fd96d0f4
dngda <opowaekeno123@gmail.com>
2021-03-27 07:39:31 +0700

    change prefix

--- HandleMsg.js
+++ HandleMsg.js
@@ -112,11 +112,13 @@ module.exports = HandleMsg = async (client, message) => {
         const stickerMetadataCrop = { pack: 'Created with', author: 'SeroBot' }
 
         // Bot Prefix
-        body = (type === 'chat' && body.startsWith(prefix)) ? body : ((type === 'image' && caption || type === 'video' && caption) && caption.startsWith(prefix)) ? caption : ''
+        regex = /^\/|^!|^~|^@|^#|^\$|^%|^&|^\+|^\?|^;|^:|^=/g
+        body = (type === 'chat' && body.replace(regex, '/').startsWith(prefix)) ? body.replace(regex, '/') : ((type === 'image' && caption || type === 'video' && caption) && caption.replace(regex, '/').startsWith(prefix)) ? caption.replace(regex, '/') : ''
         const command = body.slice(1).trim().split(/ +/).shift().toLowerCase()
         const arg = body.trim().substring(body.indexOf(' ') + 1)
         const args = body.trim().split(/ +/).slice(1)
         const isCmd = body.startsWith(prefix)
+
         const url = args.length !== 0 ? args[0] : ''
 
         const isQuotedImage = quotedMsg && quotedMsg.type === 'image'
