180da273be806a97aee2d41d6f25ef065242e38b
dngda <opowaekeno123@gmail.com>
2021-04-01 19:20:58 +0700

    .

--- restart.cmd
+++ restart.cmd
@@ -1,2 +1,6 @@
-taskkill /IM "node.exe" /F
+@ECHO OFF
+FOR /f "tokens=2" %%a in ('
+    tasklist /FI "IMAGENAME eq node.exe" ^| findstr /ic:"node.exe"
+') Do SET pid=%%a
 START "" node index
+taskkill /F /PID %pid%
