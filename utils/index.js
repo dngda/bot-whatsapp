8a31db9d0e5af61b35e09dc70668d465ed5d8c2a
dngda <opowaekeno123@gmail.com>
2021-04-02 11:47:19 +0700

    change toDHMS

--- utils/index.js
+++ utils/index.js
@@ -137,12 +137,8 @@ String.prototype.toDHms = function () {
     var minutes = Math.floor((sec_num - (hours * 3600)) / 60)
     var seconds = sec_num - (hours * 3600) - (minutes * 60)
     var days = 0
-
     if (hours >= 24) {days = Math.floor(hours / 24); hours = hours % 24}
-    if (hours   < 10) {hours   = "0"+hours}
-    if (minutes < 10) {minutes = "0"+minutes}
-    if (seconds < 10) {seconds = "0"+seconds}
-    var time    = days+' day(s) '+hours+' hour(s) '+minutes+' minute(s) '+seconds+' sec(s)'
+    var time    = days+' days '+hours+' hours '+minutes+' minutes '+seconds+' secs'
     return time
 }
 
