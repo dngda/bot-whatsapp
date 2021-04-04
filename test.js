121a8ef92f102f6ab11c62dde2d67a56b8cbd93d
dngda <opowaekeno123@gmail.com>
2021-04-04 19:15:20 +0700

    save schJob to json

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
-const schedule = require('node-schedule')
+const {
+	futureMilis,
+	loadJob
+} = require('./lib/schedule')
 
-const when = new Date(Date.now() + 5000)
-const job = schedule.scheduleJob(when, function(fireDate){
-  console.log('This job was supposed to run at ' + fireDate + ', but actually ran at ' + new Date())
-})
+futureMilis()
