0b1ddd4476befa086502018ce7f3c8c32e0e4185
dngda <opowaekeno123@gmail.com>
2021-04-08 13:54:03 +0700

    iseh reminder

--- HandleMsg.js
+++ HandleMsg.js
@@ -1346,7 +1346,7 @@ const HandleMsg = async (client, message, browser) => {
 
                     case 'reminder':
                     case 'remind': {
-                        if (args.length === 0 && !message.hasOwnProperty('quotedMsg')) return client.reply(from, `Kirim pesan pada waktu tertentu.\n${prefix}remind <Waktu> <Text atau isinya>\nMisal waktu 1d1h1m = 1 hari lebih 1 jam lebih 1 menit\nContoh: ${prefix}remind 1h5m Jangan Lupa minum!\nBot akan kirim ulang pesan 'Jangan Lupa minum!' setelah 1 jam 5 menit.\n\n${prefix}remind <DD/MM-hh:mm> <Text atau isinya>\nContoh: ${prefix}remind 15/04-12:00 Jangan Lupa minum!\nBot akan kirim ulang pesan 'Jangan Lupa minum!' Pada tanggal 15 bulan 4 jam 12:00 tahun sekarang.`, id)
+                        if (args.length === 0 && !message.hasOwnProperty('quotedMsg')) return client.reply(from, `Kirim pesan pada waktu tertentu.\n*${prefix}remind <xdxhxm> <Text atau isinya>*\nIsi x dengan angka. Misal 1d1h1m = 1 hari lebih 1 jam lebih 1 menit\nContoh: ${prefix}remind 1h5m Jangan Lupa minum!\nBot akan kirim ulang pesan 'Jangan Lupa minum!' setelah 1 jam 5 menit.\n\n*${prefix}remind <DD/MM-hh:mm> <Text atau isinya>* utk tgl dan waktu spesifik\n*${prefix}remind <hh:mm> <Text atau isinya>* utk waktu pd hari ini\nContoh: ${prefix}remind 15/04-12:00 Jangan Lupa minum!\nBot akan kirim ulang pesan 'Jangan Lupa minum!' pada tanggal 15/04 jam 12:00 tahun sekarang.`, id)
                         const dd = args[0].match(/\d+(d|D)/g)
                         const hh = args[0].match(/\d+(h|H)/g)
                         const mm = args[0].match(/\d+(m|M)/g)
