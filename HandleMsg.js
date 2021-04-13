bc2814802ab41944ab0a06df13cfecfda7c65648
dngda <opowaekeno123@gmail.com>
2021-04-14 05:43:34 +0700

    .

--- HandleMsg.js
+++ HandleMsg.js
@@ -832,9 +832,8 @@ const HandleMsg = async (client, message, browser) => {
                         const result = await zrapi.keeptiktok(arg).catch(err => client.reply(from, resMsg.error.norm, id).then(() => console.log(err)))
                         const req = https.get(result, (res) => {
                             res.pipe(file)
+                            await client.sendFile(from, path, `{t}.mp4`, '', id).then(() => fs.unlinkSync(path)).catch(err => client.reply(from, resMsg.error.norm, id).then(() => console.log(err)))
                         })
-                        await client.sendFile(from, path, `{t}.mp4`, '', id).catch(err => client.reply(from, resMsg.error.norm, id).then(() => console.log(err)))
-                        fs.unlinkSync(path)
                         break
                     }
 
