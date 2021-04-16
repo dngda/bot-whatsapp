e18a1035b7cb0f859a81bdcfd1c67b26a52cab9c
dngda <opowaekeno123@gmail.com>
2021-04-16 22:49:19 +0700

    fix nulis

--- HandleMsg.js
+++ HandleMsg.js
@@ -463,8 +463,7 @@ const HandleMsg = async (client, message, browser) => {
                         if (args.length == 0 && !isQuotedChat) return client.reply(from, `Membuat bot menulis teks yang dikirim menjadi gambar\nPemakaian: ${prefix}nulis [teks]\n\ncontoh: ${prefix}nulis i love you 3000`, id)
                         const content = isQuotedChat ? quotedMsgObj.content.toString() : arg
                         const ress = await api.tulis(content)
-                        if (ress.status != 200) return client.reply(from, `Maaf terjadi error atau terdapat huruf simbol atau emoji yang tidak sesuai.`, id)
-                        await client.sendImage(from, ress.result, '', ``, id)
+                        await client.sendImage(from, ress, '', ``, id)
                             .catch((e) => {
                                 console.log(e)
                                 client.reply(from, resMsg.error.norm, id)
