4f45d2f2d1a3dc3e937d4d9e84c5b220723e7114
dngda <opowaekeno123@gmail.com>
2021-04-18 20:58:19 +0700

    remove = prefix

--- HandleMsg.js
+++ HandleMsg.js
@@ -142,7 +142,7 @@ const HandleMsg = async (client, message, browser) => {
         const stickerMetadataCrop = { pack: 'Created with', author: 'SeroBot' }
 
         // Bot Prefix
-        regex = /(^\/|^!|^\$|^%|^&|^\+|^=|^\.|^,|^<|^>|^-|^\\)(?=\w+)/g
+        regex = /(^\/|^!|^\$|^%|^&|^\+|^\.|^,|^<|^>|^-|^\\)(?=\w+)/g
         body = (type === 'chat' && body.replace(regex, '/').startsWith(prefix)) ? body.replace(regex, '/') : ((type === 'image' && caption || type === 'video' && caption) && caption.replace(regex, '/').startsWith(prefix)) ? caption.replace(regex, '/') : ''
         const command = body.slice(1).trim().split(/ +/).shift().toLowerCase()
         const arg = body.trim().substring(body.indexOf(' ') + 1)
