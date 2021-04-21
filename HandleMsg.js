fa28dc2a18abc0df5aae5d74f88ceb3d78377c7a
dngda <opowaekeno123@gmail.com>
2021-04-21 22:45:50 +0700

    -

--- HandleMsg.js
+++ HandleMsg.js
@@ -1098,7 +1098,7 @@ const HandleMsg = async (client, message, browser) => {
                     case 'lyric': {
                         if (args.length === 0) return client.reply(from, `Untuk mencari lirik dengan nama lagu atau potongan lirik\nketik: ${prefix}lirik <qeury>\nContoh: ${prefix}lirik lathi`, id)
                         let res = await api.lirik(arg).catch(err => client.reply(from, resMsg.error.norm, id).then(() => console.log(err)))
-                        await client.reply(from, res, id)
+                        await client.reply(from, res.lirik, id)
                         break
                     }
 
