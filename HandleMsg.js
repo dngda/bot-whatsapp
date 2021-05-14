3b4dfc073aa5ca92839cc3678390dad03f5fec6d
dngda <opowaekeno123@gmail.com>
2021-05-15 00:02:37 +0700

    try

--- HandleMsg.js
+++ HandleMsg.js
@@ -924,6 +924,33 @@ const HandleMsg = async (client, message, browser) => {
                         break
                     }
 
+                    case 'investigasi': {
+                        if(!isQuotedPtt && !isQuotedAudio) return client.reply(from, `Silakan reply audio atau voice notes dengan perintah ${prefix}investigasi`, id)
+                        const _inp = await decryptMedia(quotedMsg)
+
+                        var time = moment(t * 1000).format('mmss')
+                        var inpath = `./media/ininvestigasi_${time}.mp3`
+                        var outpath = `./media/outinvestigasi_${time}.mp3`
+                        fs.writeFileSync(inpath, _inp)
+
+                        ffmpeg(inpath)
+                            .setFfmpegPath('./bin/ffmpeg')
+                            .complexFilter(`rubberband=pitch=1.5`)
+                            .on('error', (err) => {
+                                console.log('An error occurred: ' + err.message)
+                                fs.unlinkSync(inpath)
+                                fs.unlinkSync(outpath)
+                                return client.reply(from, resMsg.error.norm, id)
+                              })
+                            .on('end', () => {
+                                client.sendFile(from, outpath,'investigasi.mp3','', id).then(console.log(color('[LOGS]', 'grey'), `Audio Processed for ${processTime(t, moment())} Second`))
+                                fs.unlinkSync(inpath)
+                                fs.unlinkSync(outpath)
+                              })
+                            .saveToFile(outpath)
+                        break
+                    }
+
                     case 'nightcore': {
                         if(!isQuotedPtt && !isQuotedAudio) return client.reply(from, `Silakan reply audio atau voice notes dengan perintah ${prefix}nightcore`, id)
                         const _inp = await decryptMedia(quotedMsg)
