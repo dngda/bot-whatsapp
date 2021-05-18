08c0b0123b8844aaf618f85812acd8f2ef02dab9
dngda <opowaekeno123@gmail.com>
2021-05-18 20:14:28 +0700

    .

--- HandleMsg.js
+++ HandleMsg.js
@@ -1035,7 +1035,7 @@ const HandleMsg = async (client, message, browser) => {
 
                         ffmpeg(inpath)
                             .setFfmpegPath('./bin/ffmpeg')
-                            .audioFilters('atempo=0.9,asetrate=44100*0.7,firequalizer=gain_entry=\'entry(0,10);entry(250,5);entry(1000,0);entry(4000,0);entry(16000,0)\'')
+                            .audioFilters('atempo=0.94,asetrate=44100*0.7,firequalizer=gain_entry=\'entry(0,7);entry(250,3);entry(1000,0);entry(4000,0);entry(16000,0)\'')
                             .on('error', (err) => {
                                 console.log('An error occurred: ' + err.message)
                                 fs.unlinkSync(inpath)
