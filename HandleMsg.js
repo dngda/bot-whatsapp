c1c22ac67a1481f7b2c6deb82629cf370c37c030
dngda <opowaekeno123@gmail.com>
2021-05-18 19:48:23 +0700

    s

--- HandleMsg.js
+++ HandleMsg.js
@@ -2218,8 +2218,10 @@ const HandleMsg = async (client, message, browser) => {
         
         // Anti link group function
         if (isAntiLinkGroup && isGroupMsg){
+            console.log(color('[LOGS]', 'grey'), `Group anti link`)
             if (body.match(/chat\.whatsapp\.com/gi) !== null) {
-            if (!isBotGroupAdmins) return client.sendText(from, 'Gagal melakukan kick, bot bukan admin')
+                console.log(color('[LOGS]', 'grey'), `Group link detected, kicking sender...`)
+                if (!isBotGroupAdmins) return client.sendText(from, 'Gagal melakukan kick, bot bukan admin')
                 client.reply(from, `Link group whatsapp terdeteksi! Auto kick...`, id)
                 await client.removeParticipant(groupId, pengirim)
             }
