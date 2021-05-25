5ec09d1e51e8db6e9fc244ba076412b2176e94f3
dngda <opowaekeno123@gmail.com>
2021-05-25 10:55:28 +0700

    upd msg

--- HandleMsg.js
+++ HandleMsg.js
@@ -446,7 +446,7 @@ const HandleMsg = async (client, message, browser) => {
                                     client.reply(from, 'Maaf terjadi error atau filenya terlalu besar!', id)
                                 })
                         } else {
-                            await client.reply(from, `Tidak ada gambar/video!\nUntuk menggunakan ${prefix}sticker, kirim gambar/reply gambar atau file png dengan caption\n*${prefix}sticker* (biasa uncrop)\n*${prefix}sticker crop* (square crop)\n*${prefix}sticker circle* (circle crop)\n*${prefix}sticker nobg* (tanpa background)\n\natau Kirim pesan dengan\n*${prefix}sticker <link_gambar>*\nTanpa simbol <>\nUntuk membuat sticker animasi. Kirim video atau reply/quote video dengan caption *${prefix}sticker* max 8 secs. Selebihnya akan dipotong otomatis`, id)
+                            await client.reply(from, `Tidak ada gambar/video!\nUntuk menggunakan ${prefix}sticker, kirim gambar/reply gambar atau *file png/webp* dengan caption\n*${prefix}sticker* (biasa uncrop)\n*${prefix}sticker crop* (square crop)\n*${prefix}sticker circle* (circle crop)\n*${prefix}sticker nobg* (tanpa background)\n\natau Kirim pesan dengan\n*${prefix}sticker <link_gambar>*\n\nUntuk membuat *sticker animasi.* Kirim video/gif atau reply/quote video/gif dengan caption *${prefix}sticker* max 8 secs. Selebihnya akan dipotong otomatis`, id)
                         }
                         break
                     }
