58118d27ffc1d8befcf8beb5b39cbffb2e5e2423
dngda <opowaekeno123@gmail.com>
2021-06-03 17:13:31 +0700

    upd

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ let {
     prefix
 } = setting
 
-const queue = new PQueue({ concurrency: 4, timeout: 10000, throwOnTimeout: true })
+const queue = new PQueue({ concurrency: 8, timeout: 10000, throwOnTimeout: true })
 queue.on('next', () => {
     if (queue.size > 0 || queue.pending > 0) console.log(color('[==>>]', 'red'), `In-process: ${queue.pending} In-queue: ${queue.size}`)
 })
