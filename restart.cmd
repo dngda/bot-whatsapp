2773f3446da403b2f15362353e4bed3d20ace080
dngda <opowaekeno123@gmail.com>
2021-06-09 14:56:32 +0700

    okey

--- restart.cmd
+++ restart.cmd
@@ -2,5 +2,5 @@
 FOR /f "tokens=2" %%a in ('
     tasklist /FI "IMAGENAME eq node.exe" ^| findstr /ic:"node.exe"
 ') Do SET pid=%%a
-START "" node index
+START "Bot WhatsApp" node index
 taskkill /F /PID %pid%
