06f08c4dc9982706957ffa0d06e83ac7e2f93f5a
dngda <opowaekeno123@gmail.com>
2021-07-02 01:15:28 +0700

    ⚙ pm2

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -4,15 +4,17 @@ module.exports = {
     name: "Bot WhatsApp",
     script: "./index.js",
     watch: "true",
+    args: ["--color"],
+    node_args: '--harmony',
+    error_file: './logs/pm2/XXXerr.log',
+    combine_logs: true,
+    cron_restart: '30 */6 * * *',
     ignore_watch: [
       "data",
       "node_modules",
       "media",
       "logs",
       ".*"
-    ],
-    args: ["--color"],
-    error_file: './logs/pm2/XXXerr.log',
-    combine_logs: true
+    ]
   }]
 }
